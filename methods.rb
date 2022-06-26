bba74652205219208ca0576b0319d1981fc96e90
Lyn <lynfinman@gmail.com>
2022-06-26 18:11:38 -0400

    commit tree c7430dbb5c359aadef393da0af9e275943debef1

--- methods.rb
+++ methods.rb
@@ -1,1 +1,22 @@
 # Your code here!
+def greet_programmer
+  puts 'Hello, programmer!'
+end
+
+def greet(p_name)
+  puts "Hello, #{p_name}!"
+end
+
+def greet_with_default(g_name = 'programmer')
+  puts "Hello, #{g_name}!"
+end
+
+def add(num_1, num_2)
+  num_1 + num_2
+end
+
+def halve(num)
+  return nil if num.class != Integer
+
+  num / 2
+end
---
+++ sample.rb
@@ -0,0 +1,13 @@
+def my_function(param)
+  puts 'Running my_function'
+  param + 1
+end
+
+result = my_function 3
+puts result
+
+def test_just_string
+  'Bob Ross'
+  puts 'Bob Ross'
+end
+test_just_string
